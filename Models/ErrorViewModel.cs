8930be0e68e14630e239e26614b88061b7143577
Nikkkt <work.terpilovsky@gmail.com>
2024-06-28 18:24:52 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_Classwork.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
